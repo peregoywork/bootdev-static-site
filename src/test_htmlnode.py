d05ca5942f152773a244f49b6d8726f5eb845778
matthew bootdev <peregoywork@gmail.com>
2026-07-18 10:37:33 -0700

    test coverage for LeafNode

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,5 @@
 import unittest
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -25,6 +25,34 @@ class TestHTMLNode(unittest.TestCase):
     def test_can_print(self):
         node = HTMLNode(props={ "href": "https://www.google.com", "target": "_blank" })
         print(node)
+
+class TestLeafNode(unittest.TestCase):
+    def test_create_leaf(self):
+        leaf_node = LeafNode(None, "testing!")
+        leaf_node = LeafNode("", "testing me twice!")
+        leaf_node = LeafNode("a", "link me")
+        leaf_node = LeafNode("a", "link me", {"href": "boot.dev"})
+        assert True == True # no errors thrown
+
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+    
+    def test_leaf_to_html_no_tag(self):
+        node = LeafNode(None, "Hello, world!")
+        self.assertEqual(node.to_html(), "Hello, world!")
+
+    def test_leaf_to_html_no_value_raises(self):
+        node = LeafNode("p", None)
+        with self.assertRaises(ValueError):
+            node.to_html()
+
+    def test_leaf_repr(self):
+        node = LeafNode("span", "some text", {"class": "highlight"})
+        self.assertEqual(
+            node.__repr__(),
+            "LeafNode(span, some text, {'class': 'highlight'})",
+        )   
 
 
 if __name__=='__main__':
