7836894c81ed65615d5c27def41fc53688a29e60
matthew bootdev <peregoywork@gmail.com>
2026-07-27 19:43:50 -0700

    markdown to html and tests

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,5 +1,7 @@
 from enum import Enum
-
+from htmlnode import ParentNode, LeafNode
+from textnode import TextNode, TextType, text_node_to_html_node
+from inline_markdown import text_to_textnodes
 
 class BlockType(Enum):
     PARAGRAPH = "paragraph"
@@ -46,3 +48,68 @@ def block_to_block_type(block: str) -> BlockType:
             i += 1
         return BlockType.ORDERED_LIST
     return BlockType.PARAGRAPH
+
+
+def text_to_code(text: str):
+    text = text[4:-3]
+    inner_node = TextNode(text, TextType.CODE)
+    html_node = text_node_to_html_node(inner_node)
+    return html_node
+
+def text_to_quoteblock(text: str):
+    text = text.replace('> ', '').replace('\n', ' ')
+    return text_to_children(text)
+
+def text_to_unordered_list(text: str):
+    return [
+        ParentNode('li', text_to_children(line[2:].strip()))
+        for line in text.split('\n') 
+    ]
+
+def text_to_ordered_list(text: str):
+    return [
+        ParentNode('li', text_to_children(line.split('.')[1].strip()))
+        for line in text.split('\n') 
+    ]
+
+def text_to_children(text: str):
+    textnodes = text_to_textnodes(text.replace('\n', ' '))
+    htmlnodes = [text_node_to_html_node(t) for t in textnodes]
+    return htmlnodes
+
+def block_to_html_node(block, block_type):
+    match block_type:
+        case BlockType.PARAGRAPH:
+            children = text_to_children(block)
+            return ParentNode('p', children)
+        case BlockType.HEADING:
+            level = block.split(' ')[0].count('#')
+            block = block[level + 1:]
+            children = text_to_children(block)
+            return ParentNode(f'h{level}', children)
+        case BlockType.CODE:
+            code_node = text_to_code(block)
+            return ParentNode('pre', [code_node])
+        case BlockType.QUOTE:
+            quote_node = text_to_quoteblock(block)
+            return ParentNode('blockquote', quote_node)
+        case BlockType.UNORDERED_LIST:
+            list_nodes = text_to_unordered_list(block)
+            return ParentNode('ul', list_nodes)
+        case BlockType.ORDERED_LIST:
+            list_nodes = text_to_ordered_list(block)
+            return ParentNode('ol', list_nodes)
+
+def markdown_to_html_node(markdown: str) -> ParentNode:
+    blocks = markdown_to_blocks(markdown)
+    child_nodes = []
+    for block in blocks:
+        if block == "":
+            continue
+        block_type = block_to_block_type(block)
+        node = block_to_html_node(block, block_type)
+        child_nodes.append(node)
+    return ParentNode('div', child_nodes)
+
+
+
