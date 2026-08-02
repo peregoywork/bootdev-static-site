fbfe52897971e3f18c64fab883602d5a7e1b629c
matthew bootdev <peregoywork@gmail.com>
2026-08-01 22:30:30 -0700

    get basepath and pass to gen funcs, ensure dirs exist

--- src/main.py
+++ src/main.py
@@ -1,21 +1,41 @@
 from textnode import TextNode
-from build_public import clean_directory, copy_directory, generate_pages_recursive
+from build_public import (
+    clean_directory, 
+    copy_directory, 
+    generate_pages_recursive, 
+    ensure_exists
+)
 from pathlib import Path
+import sys
 import logging
+
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+
 BASE_PATH = Path(__file__).resolve().parent.parent
 STATIC_DIR = BASE_PATH / "static"
-PUBLIC_DIR = BASE_PATH / "public" 
+DOCS_DIR = BASE_PATH / "docs"
 CONTENT_DIR = BASE_PATH / "content"
 
 
+def get_basepath():
+    args = sys.argv
+    if len(args) > 1:
+        return args[1]
+    return '/'
+
+
 def main():
-    clean_directory(PUBLIC_DIR)
-    copy_directory(STATIC_DIR, PUBLIC_DIR)
-    generate_pages_recursive(CONTENT_DIR, 'template.html', PUBLIC_DIR)
+    basepath = get_basepath()
+
+    print(basepath)
+
+    ensure_exists([STATIC_DIR, DOCS_DIR, CONTENT_DIR])    
+    clean_directory(DOCS_DIR)
+    copy_directory(STATIC_DIR, DOCS_DIR)
+    generate_pages_recursive(CONTENT_DIR, 'template.html', DOCS_DIR, basepath)
 
 
 if __name__=='__main__':
